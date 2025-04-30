763672332abd8331d0af05d8a3406afdbfc30c01
Ryunopasiri <ryunopasiri@gmail.com>
2025-04-30 10:32:45 +0900

    新規ファイル追加
    ここは詳細な説明を書くとよいです

---
+++ mydiary.java
@@ -0,0 +1,10 @@
+package part1;
+
+public class mydiary {
+    public static void main(String[] args) {
+        System.out.println("日記ソフト");
+        System.out.println("終了します");
+        
+    }
+    
+}
