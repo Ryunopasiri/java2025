9612200c9c99ea224c0ecdeccb90f9c59ca69e90
Ryunopasiri <ryunopasiri@gmail.com>
2025-05-07 10:31:24 +0900

    追加したにょ

---
+++ j0507/Main.java
@@ -0,0 +1,15 @@
+package j0507;
+
+public class Main {     // クラスブロック
+    public static void main(String[] args) {    //メソッドブロック
+        //変数宣言時に、型を宣言
+        int age = 19;   //年齢を保存する変数
+        final double PI = 3.14;   //円周率
+        String msg = "Hello World"; //文字列
+
+        System.out.println(age);
+        System.out.println(PI);
+        System.out.println(msg);
+    }
+}
+    
---
+++ j0507/Main1_2.java
@@ -0,0 +1,14 @@
+package j0507;
+
+public class Main1_2 {
+    public static void main(String[] args) {
+        //変数aに3代入
+        int a = 3;
+        //変数bに5代入
+        int b = 5;
+        //変数cにa * bの結果を代入
+        int c = a * b;
+        //cを出力
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    }
+}
